7d1dc07492227a7fe1bef8f7e8c06eac84db5697
Kyler <46557883+HiFiiDev@users.noreply.github.com>
2019-09-06 21:04:53 -0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -14,13 +14,13 @@ echo "Let's begin by setting up the arch"
 export ARCH=arm64
 
 #export lz4
-export LZ4_PREBUILTS_BIN=~/pacrom/prebuilts/misc/linux-x86/lz4
+export LZ4_PREBUILTS_BIN=~/lineage/prebuilts/misc/linux-x86/lz4
 
 #export cc
-export CROSS_COMPILE=~/pacrom/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=~/lineage/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 
 #export cc32
-export CROSS_COMPILE_ARM32=~/pacrom/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
+export CROSS_COMPILE_ARM32=~/lineage/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 
 #make proper
 make mrproper
@@ -33,9 +33,9 @@ make
 
 echo "kernel build complete - now let's move it!"
 #remove the old one
-rm -rf ~/pacrom/device/google/marlin-kernel/Image.lz4-dtb
+rm -rf ~/lineage/device/google/marlin-kernel/Image.lz4-dtb
 
 #copy the new one
-cp ~/pacrom/kernel/google/marlin/arch/arm64/boot/Image.lz4-dtb ~/pacrom/device/google/marlin-kernel
+cp ~/lineage/kernel/google/marlin/arch/arm64/boot/Image.lz4-dtb ~/pacrom/device/google/marlin-kernel
 
 
